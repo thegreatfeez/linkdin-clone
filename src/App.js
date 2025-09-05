ca6e7ece458a18abbdaeae613493d717355fade9
thegreatfeez <adamsafeez4@gmail.com>
2025-09-05 05:27:35 +0100

    Login UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
