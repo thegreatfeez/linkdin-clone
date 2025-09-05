fd106d5773e2a14c11ddde167d863b6852534d1e
thegreatfeez <adamsafeez4@gmail.com>
2025-09-05 12:51:15 +0100

    Other Ui Component done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={
+            <>
+              <Header />
+              <Home />
+            </>
+          } />
         </Routes>
       </BrowserRouter>
     </div>
